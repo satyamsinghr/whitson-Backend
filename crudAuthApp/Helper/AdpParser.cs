053bcb55422c12c70bb171037d27f3fd939986e0
ShubhamSingh <shubham.singh.10193@gmail.com>
2023-11-16 14:40:44 +0530

    commit

---
+++ crudAuthApp/Helper/AdpParser.cs
@@ -0,0 +1,21 @@
+﻿namespace crudAuthApp.Helper
+{
+    public class AdpParser
+    {
+        public async Task Parse()
+        {
+            string fileName = @"C:\Users\HP\Downloads\MDUNP25P.A01";
+
+            using (StreamReader streamReader = System.IO.File.OpenText(fileName))
+            {
+                string text = streamReader.ReadToEnd();
+                string[] lines = text.Split(Environment.NewLine);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
